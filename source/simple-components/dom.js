b58938c6451064b80f2dca40d2acb5cc406f5a02
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-25 19:38:28 +0200

    → Lesson 7. Checkpoint at: 20:29:05

--- source/simple-components/dom.js
+++ source/simple-components/dom.js
@@ -1,6 +1,8 @@
 export default (text = 'Привет! 🎉🎉🎉') => {
     const element = document.createElement('div');
 
+    import('./javascript'); // returns promise
+
     element.innerHTML = text;
 
     return element;
