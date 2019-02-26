7a2f083eb3e0837f7f847fe58cd2a452e64b3b2d
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-26 16:05:14 +0200

    → Lesson 7. Checkpoint at: 18:00:05

--- source/react-component/Carousel/Image/index.js
+++ source/react-component/Carousel/Image/index.js
@@ -4,10 +4,10 @@ import React from 'react';
 // Styles
 import Styles from './styles.css';
 
-export const Image = props => {
+export const Image = (props) => {
     return (
-        <div className={Styles.image}>
-            <img src={props.link} />
+        <div className = { Styles.image }>
+            <img src = { props.link } />
         </div>
     );
 };
