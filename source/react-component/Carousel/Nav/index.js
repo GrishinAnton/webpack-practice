1bf360db16dde50db72da6cf4b4dcf95b047a82a
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-26 16:05:14 +0200

    → Lesson 7. Checkpoint at: 18:00:58

--- source/react-component/Carousel/Nav/index.js
+++ source/react-component/Carousel/Nav/index.js
@@ -1,18 +1,16 @@
 // Core
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 // Styles
 import Styles from './styles.css';
 
-export class Nav extends Component {
-    render() {
-        return (
-            <nav className = { Styles.nav }>
-                <Link to = '/space'>Space</Link>
-                <Link to = '/alien'>Alien</Link>
-                <Link to = '/jellyfish'>Jellyfish</Link>
-            </nav>
-        );
-    }
-}
+export const Nav = () => {
+    return (
+        <nav className = { Styles.nav }>
+            <Link to = '/space'>Space</Link>
+            <Link to = '/alien'>Alien</Link>
+            <Link to = '/jellyfish'>Jellyfish</Link>
+        </nav>
+    );
+};
