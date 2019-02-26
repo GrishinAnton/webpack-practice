9747f14eca6c83e988a5b7b839eb2efb4846c21b
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-26 16:05:14 +0200

    → Lesson 7. Checkpoint at: 17:53:35

--- scripts/webpack/constants.js
+++ scripts/webpack/constants.js
@@ -6,6 +6,7 @@ import { path as PROJECT_ROOT } from 'app-root-path';
 export const HOST = 'localhost';
 export const PORT = 3000;
 
+// Paths
 export { PROJECT_ROOT };
 export const SOURCE_DIRECTORY = resolve(PROJECT_ROOT, './source');
 export const BUILD_DIRECTORY = resolve(PROJECT_ROOT, './build');
