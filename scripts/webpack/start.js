6c8c831d1b412c1bb9cb9eb46b234f96a15955c9
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-26 16:05:14 +0200

    → Lesson 7. Checkpoint at: 18:20:55

--- scripts/webpack/start.js
+++ scripts/webpack/start.js
@@ -18,6 +18,8 @@ import DevServer from 'webpack-dev-server';
 import hot from 'webpack-hot-middleware';
 import chalk from 'chalk'; // Раскрашивает консоль;
 import openBrowser from 'react-dev-utils/openBrowser';
+import { choosePort } from 'react-dev-utils/WebpackDevServerUtils';
+
 
 // Config
 import getDevConfig from './config/webpack.dev';
@@ -32,7 +34,7 @@ const compiler = webpack(getDevConfig());
 
 (async () => {
     try {
-        const choosenPort = await choosePort(PORT);
+        const choosenPort = await choosePort(HOST, PORT);
 
         if (!choosenPort) {
             console.log(
