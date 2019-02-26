196d82e004be2126c6be5be92670faff3b405cce
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-26 16:05:14 +0200

    → Lesson 7. Checkpoint at: 16:33:59

--- scripts/webpack/modules/optimization.js
+++ scripts/webpack/modules/optimization.js
@@ -78,7 +78,7 @@ export const optimizeBuild = () => ({
         // Эта опция включена всегда. Когфигурируется в SplitChunksPlugin.
         splitChunks: {
             // Режим разделения кода. По-умолчанию — async.
-            chunks:                 'all', // initial, all (async + initial)
+            chunks:                 'async', // initial, all (async + initial)
             // Минимальный размер нового чанка для отделения.
             minSize:                30000, // bytes
             // Максимальный размер нового чанка для отделения.
