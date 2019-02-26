2ac255ad5ca8315b35dbe4f9857e451671c1d415
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-26 16:05:14 +0200

    → Lesson 7. Checkpoint at: 18:12:06

--- scripts/webpack/config/webpack.common.js
+++ scripts/webpack/config/webpack.common.js
@@ -1,9 +1,8 @@
 // Core
 import merge from 'webpack-merge';
 
-// Constants
-import { SOURCE_DIRECTORY, BUILD_DIRECTORY } from '../constants';
-
+// Instruments
+import { BUILD_DIRECTORY } from '../constants';
 // Modules
 import * as modules from '../modules';
 
@@ -15,7 +14,6 @@ import * as modules from '../modules';
  */
 export default () => {
     const { NODE_ENV } = process.env;
-    const IS_DEVELOPMENT = NODE_ENV === 'development';
 
     return merge(
         {
@@ -30,7 +28,11 @@ export default () => {
                 hashDigestLength: 5,
             },
         },
-        modules.defineEnvVariables(),
+        modules.defineEnvVariables({
+            __ENV__:  JSON.stringify(NODE_ENV),
+            __DEV__:  NODE_ENV === 'development',
+            __PROD__: NODE_ENV === 'production',
+        }),
         modules.loadJavaScript(),
         modules.loadSass(),
         modules.loadFonts(),
