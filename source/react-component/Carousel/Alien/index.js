c1da55a7838f3aa08d46767ed8ff14bd371ce81d
Dima Vakatsiienko <dvakatsiienko@lectrum.io>
2019-02-26 16:05:14 +0200

    → Lesson 7. Checkpoint at: 18:00:32

--- source/react-component/Carousel/Alien/index.js
+++ source/react-component/Carousel/Alien/index.js
@@ -1,9 +1,9 @@
 // Core
-// import React from 'react';
+import React from 'react';
 import Loadable from 'react-loadable';
 
 // Components
-import { Loading } from '../../Loading';
+import { Loading } from '../Loading';
 
 const LoadableComponent = Loadable({
     loader:  () => import(/* webpackChunkName: "alien" */ './Component'),
